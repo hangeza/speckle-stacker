115f733bc34ba5d8f866af6a30f92d308aa17f3c
hangeza <zaunick@exp2.physik.uni-giessen.de>
2025-03-31 23:48:24 +0200

    debugging failing test

--- array2.h
+++ array2.h
@@ -567,8 +567,8 @@ template <typename T>
 void Array2<T>::shift(const DimVector<int, 2>& distance)
 {
     Array2<T> shifted_arr(std::move(this->shifted(distance)));
+    this->data().reset(shifted_arr.data());
 //     std::swap(shifted_arr.data(), (*this).data());
-    *this = shifted_arr;
 }
 
 template <typename T>
