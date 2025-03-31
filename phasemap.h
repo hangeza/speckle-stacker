7e0d49b57e9ce99a26672ffae31da7769eadb39f
hangeza <zaunick@exp2.physik.uni-giessen.de>
2025-03-31 14:01:16 +0200

    removed def of implicit copy constructor

--- phasemap.h
+++ phasemap.h
@@ -9,7 +9,6 @@ struct PhaseMapElement {
     PhaseMapElement() = default;
     PhaseMapElement(bool a_flag);
     PhaseMapElement(bool a_flag, double a_consistency);
-    PhaseMapElement(const PhaseMapElement& src) = default;
     double operator()() const { return consistency; }
     operator double() const { return consistency; }
     bool flag { false };
