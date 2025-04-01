554433f9e935cacd1d619ece5656906145f5178b
hangeza <zaunick@exp2.physik.uni-giessen.de>
2025-04-02 00:13:43 +0200

    added constant

--- utility.h
+++ utility.h
@@ -8,6 +8,7 @@ namespace smip {
 struct PhaseMapElement;
 
 static constexpr double c_2pi { 6.2831853071796E+00 }; /* 2*pi */
+static constexpr double c_sqrt_2pi { std::sqrt(6.2831853071796E+00) }; /* sqrt(2*pi) */
 
 template <concept_arithmetic T>
 T complex_phase(const std::complex<T>& a) { return (std::arg(a) + M_PI) / (M_PI * 2); }
