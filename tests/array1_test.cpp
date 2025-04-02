3fdf9570c4ca6e184430dec635b373a96e0c168f
hangeza <zaunick@exp2.physik.uni-giessen.de>
2025-04-02 10:24:00 +0200

    debugging test failure on Windows+gcc

--- tests/array1_test.cpp
+++ tests/array1_test.cpp
@@ -2,7 +2,6 @@
 
 int array1_test(int argc, char* argv[])
 {
-    return 0;
     constexpr std::size_t xsize { 10 };
     constexpr std::size_t ysize { 9 };
     smip::Array2<int> arr(xsize, ysize);
