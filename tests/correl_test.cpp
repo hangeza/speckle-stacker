1e6517244f2c5cf7b8a8ef069a3545e940f774b0
hangeza <zaunick@exp2.physik.uni-giessen.de>
2025-04-04 08:11:06 +0200

    reverted some previous debugging-related changes

--- tests/correl_test.cpp
+++ tests/correl_test.cpp
@@ -20,10 +20,9 @@ int correl_test(int /*argc*/, char* /*argv*/[])
 
     for (std::size_t row { }; row < ysize; ++row) {
         for (std::size_t col { }; col < xsize; ++col) {
-            //gaus.at({ static_cast<int>(col), static_cast<int>(row) }) = smip::Gauss<double, std::size_t>({ col, row }, gausparams);
+            gaus.at({ static_cast<int>(col), static_cast<int>(row) }) = smip::Gauss<double, std::size_t>({ col, row }, gausparams);
         }
     }
-    gaus[ysize/2][xsize/2] = 1.;
     std::random_device rd; // a seed source for the random number engine
     std::mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
     std::uniform_int_distribution<int> distrib(gaus.min_sindices().max() + static_cast<int>(sigma), gaus.max_sindices().min() - static_cast<int>(sigma) - 1);
@@ -31,12 +30,8 @@ int correl_test(int /*argc*/, char* /*argv*/[])
     smip::CrossCorrelation<double> cross_correl(gaus);
     for (auto i { 0UL }; i < N_trials; ++i) {
         const smip::DimVector<int, 2> gen_shift { distrib(gen), distrib(gen) };
-        std::cout << "shift wrt ref frame: generated [x,y] = " << gen_shift << "\n";
         auto shifted_gaus = gaus.shifted(gen_shift);
-        shifted_gaus.print();
-        smip::DimVector<int,2> meas_shift { cross_correl.operator()(shifted_gaus) };
-        std::cout << "shift wrt ref frame: measured [x,y] = " << meas_shift << "\n";
-        //         std::cout << " vs. measured  [x,y] = " << meas_shift << "\n";
+        smip::DimVector<int,2> meas_shift { cross_correl(shifted_gaus) };
         smip::DimVector<int, 2> shift_diff { meas_shift - gen_shift };
         std::cout << "shift diff (generated vs. measured) [x,y] = " << shift_diff << "\n";
         assert(std::sqrt(shift_diff * shift_diff).max() <= 0.2 * sigma);
