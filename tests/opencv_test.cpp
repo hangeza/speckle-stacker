3944f2c0f8af19fa4369b5297400949e54760cca
hangeza <zaunick@exp2.physik.uni-giessen.de>
2025-04-04 14:11:54 +0200

    added test unit source file (forgotten at last commit)

---
+++ tests/opencv_test.cpp
@@ -0,0 +1,19 @@
+#include "array2.h"
+#include "videoio.h"
+#include <iostream>
+#include <random>
+
+int opencv_test(int /*argc*/, char* /*argv*/[])
+{
+    const std::string filename { "../../data/hu940ani/hu940ani.gif" };
+    smip::FrameExtractor fe(filename);
+    std::cout << "opening video file " << filename << "\n";
+    if (!fe.is_valid()) {
+        std::cerr << "file open error: " << fe.filename() << "\n";
+        exit(-1);
+    }
+    const std::size_t nframes { fe.nframes() };
+    std::cout << "opened video file: " << nframes << " frames\n";
+    assert(nframes == 40);
+    return 0;
+}
