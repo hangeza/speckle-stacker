22ebfa2275545a8c67d9612b448945d6484ecbc3
hangeza <zaunick@exp2.physik.uni-giessen.de>
2025-04-07 13:11:01 +0200

    added concept-constrained type

--- types.h
+++ types.h
@@ -2,6 +2,7 @@
 #include <complex>
 #include <concepts>
 #include <type_traits>
+#include <valarray>
 
 namespace smip {
 
@@ -11,13 +12,17 @@ template <typename T>
 concept concept_floating = std::is_floating_point_v<T>;
 template <typename T>
 concept concept_arithmetic = std::is_arithmetic_v<T>;
-// Concept for std::valarray of arithmetic types
+
+/**
+ * @brief Concept for std::valarray of arithmetic types.
+ * This is a shorthand definition describing a concept to select only types with following properties:
+ * * a std::valarray<T> with T an arithmetic type (integral or floating type)
+ * * a type derived from std::valarray<T> with T an arithmetic type
+ */
 template <typename T>
-concept concept_valarray_of_arithmetic = requires(T x)
-{
-    typename T::value_type;
-    requires std::is_arithmetic_v<typename T::value_type>;
-};
+concept concept_valarray_of_arithmetic =
+    (std::is_same_v<T, std::valarray<typename std::decay_t<T>::value_type>> && std::is_arithmetic_v<typename std::decay_t<T>::value_type>) || 
+    (std::is_base_of_v<std::valarray<typename std::decay_t<T>::value_type>, T> && std::is_arithmetic_v<typename std::decay_t<T>::value_type>);
 
 typedef std::complex<double> complex_t;
 typedef std::complex<float> bispec_complex_t;
