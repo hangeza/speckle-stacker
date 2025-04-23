549395a32280766ef416f6effd7d790d66b571da
hangeza <zaunick@exp2.physik.uni-giessen.de>
2025-04-23 10:37:27 +0200

    debugging

--- src/utility.cpp
+++ src/utility.cpp
@@ -2,10 +2,16 @@
 #include "phasemap.h"
 
 #if defined _WIN32
-extern "C" void __attribute__((dllexport)) test_export() {}
+extern "C" void __attribute__((dllexport)) __attribute__((visibility("default"))) test_export() {}
 #endif
 
 namespace smip {
+
+#if defined _WIN32
+void force_link() {
+    test_export();
+}
+#endif
 
 double get_phase_consistency(const PhaseMapElement& pme)
 {
